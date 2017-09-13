3d977131071a7873dcedb9e349c4c1999856e028
Harry Plumer <harry.plumer@gmail.com>
2017-09-13 15:06:00 -0600

    view built

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,5 @@
 Rails.application.routes.draw do
-  get 'sushi_rolls/index'
-
-  get 'sushi_rolls/show'
-
-  get 'sushi_rolls/edit'
-
-  get 'sushi_rolls/new'
-
+  root 'sushi_rolls#index'
   devise_for :users
   resources :sushi_rolls
   
