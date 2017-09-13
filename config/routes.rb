7bbe3ae3dcab0e07f08af0c7e305b9c03fc0e873
Harry Plumer <harry.plumer@gmail.com>
2017-09-13 14:28:53 -0600

    sushi rolls controller and routes created

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+  get 'sushi_rolls/index'
+
+  get 'sushi_rolls/show'
+
+  get 'sushi_rolls/edit'
+
+  get 'sushi_rolls/new'
+
   devise_for :users
+  resources :sushi_rolls
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
